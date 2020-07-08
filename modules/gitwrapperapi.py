4ecc722a0059c00b636801ba0d2e74b5f97d0479
jevisan <jorgevigilsantos@gmail.com>
2020-07-07 21:57:27 -0500

    Implementation to display commits only in their respective branches

--- modules/gitwrapperapi.py
+++ modules/gitwrapperapi.py
@@ -15,22 +15,31 @@ def build_repo_stuct(repo):
     remotes = [{"name": str(remote), "url": remote.url} for remote in repo.remotes]
     repo_struct['remotes'] = remotes
 
+    # Workaround for exluding commits from parent branches
+    # So current branch only contains its commits
+    other_shas = set()
+
+    branch_list = repo.branches
+    branch_list.reverse() # Trick to traverse the branches in oldest-newest order
+
     branches = []
-    for branch in repo.branches:
+    for branch in branch_list:
         curr_branch = {}
         curr_branch['name'] = str(branch)
         curr_branch['commits'] = []
         commits = list(repo.iter_commits(branch))
         for commit in commits:
-            curr_commit = {}
-            curr_commit['hexsha'] = str(commit.hexsha)
-            curr_commit['summary'] = commit.summary
-            curr_commit['author'] = commit.author.name
-            curr_commit['author_email'] = commit.author.email
-            curr_commit['timestamp'] = str(commit.authored_datetime)
-            curr_commit['count'] = str(commit.count())
-            curr_commit['size'] = commit.size
-            curr_branch['commits'].append(curr_commit)
+            if commit.hexsha not in other_shas:
+                curr_commit = {}
+                curr_commit['hexsha'] = str(commit.hexsha)
+                curr_commit['summary'] = commit.summary
+                curr_commit['author'] = commit.author.name
+                curr_commit['author_email'] = commit.author.email
+                curr_commit['timestamp'] = str(commit.authored_datetime)
+                curr_commit['count'] = str(commit.count())
+                curr_commit['size'] = commit.size
+                curr_branch['commits'].append(curr_commit)
+                other_shas.add(commit.hexsha)
         branches.append(curr_branch)
 
     repo_struct['branches'] = branches
@@ -49,3 +58,7 @@ def init_repo():
     else:
         print('Could not load repository at {}'.format(repo_path))
         return None
+
+if __name__ == '__main__':
+    repo_struct = init_repo()
+    print(repo_struct)
